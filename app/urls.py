2573352fe5e4ac8d302b7f153ee9f61391655ec6
Potatinho23 <mat.aninir@duocuc.cl>
2023-10-13 23:57:05 -0300

    Login

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
 ]
+
