e66707a86bc3d406146d840d7e73caaa500dab65
samuel garrido fuentes <sam.garrido@duocuc.cl>
2023-10-29 20:54:45 -0300

    Modulo de agendar hora con js

---
+++ app/static/js/rut.js
@@ -0,0 +1,9 @@
+function dgv(T)    //digito verificador
+{  
+      var M=0,S=1;
+	  for(;T;T=Math.floor(T/10))
+      S=(S+T%10*(9-M++%6))%11;
+	  //return S?S-1:'k';
+      
+      alert(S?S-1:'k');
+ }
