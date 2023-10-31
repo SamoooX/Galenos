efcf0709901c8f10329b6169960caf2421e2f82b
samuel garrido fuentes <sam.garrido@duocuc.cl>
2023-10-30 21:20:56 -0300

    Opcion para que medico modifique su agenda

--- app/static/js/medico.js
+++ app/static/js/medico.js
@@ -1,32 +1,67 @@
 $(document).ready(function() {
-    $("#cambiar-disponibilidad").on("click", function() {
-      // Crear una lista para almacenar los datos seleccionados
-      var seleccionados = [];
-  
+  $("#no-disponible").on("click", function() {
       // Iterar sobre los checkboxes marcados
       $(".agenda-checkbox:checked").each(function() {
+          var rut = $(this).data("rut");
+          var fecha = $(this).data("fecha");
+          var hora = $(this).data("hora");
+
+          // Crear un objeto con los datos de la fecha actual
+          var data = {
+              rut_med: rut +"",
+              fecha: fecha,
+              hora: hora,
+              disponibilidad: false
+          };
+
+          // Realizar una solicitud PATCH para cambiar la disponibilidad
+          $.ajax({
+              url: 'https://galenos.samgarrido.repl.co/api/agendas/disponibilidad', // URL de la API
+              type: 'PATCH',
+              data: JSON.stringify(data),
+              contentType: 'application/json',
+              success: function(response) {
+                  // Manejar la respuesta de la API si es necesario
+                  console.log('Cambio de disponibilidad realizado para fecha:', fecha);
+              },
+              error: function(error) {
+                  console.log('Error al cambiar la disponibilidad para fecha:', fecha);
+                  console.log(error);
+              }
+          });
+      });
+  });
+
+  $("#disponible").on("click", function() {
+    // Iterar sobre los checkboxes marcados
+    $(".agenda-checkbox:checked").each(function() {
+        var rut = $(this).data("rut");
         var fecha = $(this).data("fecha");
         var hora = $(this).data("hora");
-        console.log(fecha);
-        console.log(hora);
-        // Agregar los datos al array seleccionados
-        seleccionados.push({ rut_med: "123456789", fecha: fecha, hora: hora, disponibilidad: false });
-      });
-      console.log(JSON.stringify(seleccionados));
-      // Realizar una solicitud a la API con los datos seleccionados
-      $.ajax({
-        url: 'https://galenos.samgarrido.repl.co/api/agendas/disponibilidad', // Reemplaza con la URL correcta de tu API
-        type: 'PATCH',
-        data: JSON.stringify(seleccionados),
-        contentType: 'application/json',
-        success: function(response) {
-          // Manejar la respuesta de la API si es necesario
-          console.log('Cambios de disponibilidad realizados:', response);
-        },
-        error: function(error) {
-          console.log('Error al cambiar la disponibilidad.');
-          console.log(error);
-        }
-      });
+
+        // Crear un objeto con los datos de la fecha actual
+        var data = {
+            rut_med: rut +"",
+            fecha: fecha,
+            hora: hora,
+            disponibilidad: true
+        };
+
+        // Realizar una solicitud PATCH para cambiar la disponibilidad
+        $.ajax({
+            url: 'https://galenos.samgarrido.repl.co/api/agendas/disponibilidad', // URL de la API
+            type: 'PATCH',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function(response) {
+                // Manejar la respuesta de la API si es necesario
+                console.log('Cambio de disponibilidad realizado para fecha:', fecha);
+            },
+            error: function(error) {
+                console.log('Error al cambiar la disponibilidad para fecha:', fecha);
+                console.log(error);
+            }
+        });
     });
-  });
+});
+});
